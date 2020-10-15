f2ab02840ed43f4c7bdbf6b5625ff4e78522bf5b
kaven <kaven2013@gmail.com>
2020-10-14 20:49:10 -0600

    Presentador de imagenes con argparse

--- Presentador_de_imágenes.py
+++ Presentador_de_imágenes.py
@@ -3,6 +3,8 @@
 # Se debe instalar la libreria Pillow (en Windows).
 # Para la instalación ejecutar en la terminal el comando "pip install Pillow".
 
+
+import argparse
 from PIL import Image  # Importa la librería PIL
 import time  # Importar la librería time
 
@@ -14,28 +16,45 @@ def presentar_imagen(imagen, escala):  # método que presenta la imagen
 
     if escala == "1:1":  # verifica si la escala es 1:1
         imagen.show()  # muestra la imagen
-        # otra forma es asiganrle la ruta:
-        # ruta = ("C:/Users/Henry/Desktop/" + im)
-        # luego se cambia la línea im = Image.open(im) por: im = Image.open(ruta)
 
-    if escala == "1:2":  # verifica si la escala es 1:2
+    elif escala == "1:2":  # verifica si la escala es 1:2
         a, b = imagen.size  # guarda el tamaño de la imagen
         esc1 = ((a//2), (b//2))  # aplica la escala
         imagen1 = imagen.resize(esc1)  # escala el tamaño de la imagen
         imagen1.save("meca_1.jpg")  # guarda la imagen como un archivo nuevo
         imagen1.show()
 
-    if escala == "2:1":  # verifica si la escala es 2:1
+    elif escala == "2:1":  # verifica si la escala es 2:1
         a, b = imagen.size
         esc2 = ((a*2), (b*2))
         imagen2 = imagen.resize(esc2)
         imagen2.save("meca_2.jpg")
         imagen2.show()
 
+
     t2 = time.time()  # mide el tiempo final
-    print('Tiempo del proceso: ', t2 - t1, 'Segundos')  # muestra el tiempo reauerido
+    resul = t2 - t1
+    return resul
 
 
-presentar_imagen("meca.jpg", "1:1")  # carga la imagen con escala 1:1
-presentar_imagen("meca.jpg", "1:2")  # carga la imagen con escala 1:2
-presentar_imagen("meca.jpg", "2:1")  # carga la imagen con escala 2:1
+parser = argparse.ArgumentParser(description='Presentador de imagen.')
+
+parser.add_argument('FILEPATH', type=str, help='Dirección de la imagen a escalar.')
+parser.add_argument('-e', '--escala', type=str, help='Escala a introducir, es un string. Ej: 1:1 o 1:2')
+parser.add_argument('-t', '--time', action='store_true', help='Bandera para mostrar el tiempo de ejecución.')
+
+args = parser.parse_args()
+tiempo = args.time
+
+if __name__ == '__main__':
+    if tiempo:
+        print('El tiempo de ejecucion es: ', presentar_imagen(args.FILEPATH, args.escala))
+    else:
+        presentar_imagen(args.FILEPATH, args.escala)
+
+
+
+
+# presentar_imagen("meca.jpg", "1:1")  # carga la imagen con escala 1:1
+# presentar_imagen("meca.jpg", "1:2")  # carga la imagen con escala 1:2
+# presentar_imagen("meca.jpg", "2:1")  # carga la imagen con escala 2:1
